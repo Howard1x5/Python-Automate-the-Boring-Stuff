f1ac74af77d3c25b5724009b652d52fe89008f0c
howard1x5 <cjh_719@outlook.com>
2025-02-01 11:38:20 -0700

    Added Chapter 3 functions and exercises

---
+++ chapters/chapter3/helloFunc2.py
@@ -0,0 +1,10 @@
+def hello(name):
+    print('Hello, ' + name)
+hello('Alice')
+hello('Bob')
+
+#def = define function hello
+#argument = value being passed (Alice, Bob)
+#local variable = name (does not hold parameter after return)
+#parameters = variables that have arguments assigned to them
+
