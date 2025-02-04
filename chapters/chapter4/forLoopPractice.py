314693b4c6f25be69bbc2c830643fc8456bfb896
howard1x5 <cjh_719@outlook.com>
2025-02-03 21:02:45 -0700

    Added for loop with lists practice

---
+++ chapters/chapter4/forLoopPractice.py
@@ -0,0 +1,8 @@
+# for loop practice
+# it is common to use range(len(someList))
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+    
