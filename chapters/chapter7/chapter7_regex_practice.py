167fb385481f736752d25c7c2351cbc4144ac161
howard1x5 <cjh_719@outlook.com>
2025-03-01 12:15:30 -0700

    Started phoneAndEmail.py - Create phone number regex and ToDo

--- chapters/chapter7/chapter7_regex_practice.py
+++ chapters/chapter7/chapter7_regex_practice.py
@@ -1,2 +1,32 @@
 # Placeholder for the chapter in GitHub.
 # I will also do the practice exercises in here that are not full commits. 
+
+
+""" phoneRegex = re.compile(r'((\d{3}|\(\d{3}\))?(\s|-|\.)?\d{3}(\s|-|\.)\d{4}
+(\s*(ext|x|ext.)\s*\d{2,5})?)')
+
+you can spread the regular expression over multiple lines with comments like this:
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', re.VERBOSE)
+
+Note how the previous example uses the triple-quote syntax (''') to create a multiline string so that you can spread the regular expression definition over many lines, making it much more legible.
+
+The comment rules inside the regular expression string are the same as regular Python code: the # symbol and everything after it to the end of the line are ignored. Also, the extra spaces inside the multiline string for the regular expression are not considered part of the text pattern to be matched. This lets you organize the regular expression so it’s easier to read.
+Combining re.IGNORECASE, re.DOTALL, and re.VERBOSE
+
+What if you want to use re.VERBOSE to write comments in your regular expression but also want to use re.IGNORECASE to ignore capitalization? Unfortunately, the re.compile() function takes only a single value as its second argument. You can get around this limitation by combining the re.IGNORECASE, re.DOTALL, and re.VERBOSE variables using the pipe character (|), which in this context is known as the bitwise or operator.
+
+So if you want a regular expression that’s case-insensitive and includes newlines to match the dot character, you would form your re.compile() call like this:
+
+>>> someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL)
+
+Including all three options in the second argument will look like this:
+
+>>> someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL | re.VERBOSE) """
